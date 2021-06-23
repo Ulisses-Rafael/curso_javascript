30e7184adbb636ed8c9da34a4c813e5160633df1
Ulisses <ulissesfavaris@yahoo.com.br>
2021-06-23 11:40:56 -0300

    Estruturas de repetição
    Estrutura while, e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} // Estrutura de repetição com teste lógico no início*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+// Estrutura de repetição com teste lógico no final*/
